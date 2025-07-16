7a75dba4aec05679949a2804cd695fa181c50e5d
Kerolis Khalaf <kerokhalaf59@gmail.com>
2025-07-16 04:31:29 +0200

    Move project files into CRUD-Ecommerce-Project folder

--- day 4/main.js
+++
@@ -1,9 +0,0 @@
-import { Rectangle, Square, Circle } from './shape.js';
-
-const rect = new Rectangle(5, 10);
-const square = new Square(7);
-const circle = new Circle(4);
-
-console.log(rect.toString());  
-console.log(square.toString());
-console.log(circle.toString());
